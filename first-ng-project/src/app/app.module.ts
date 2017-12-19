8552bdb6ce61fef92605c3aedb6074d191661904
NamiDorama <yulliavoitiuk@gmail.com>
2017-12-19 18:18:47 +0200

    19.12 added new files

--- first-ng-project/src/app/app.module.ts
+++ first-ng-project/src/app/app.module.ts
@@ -4,24 +4,24 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { ShoppingListComponent } from './shoppingList/shoppingList.component';
-import {ShopList} from './shoppingList/list/list.component';
-import {ListEditComponent} from './shoppingList/listEdit/listEdit.component';
-import {RecipeBookComponent} from './recipeBook/recipe.component';
-import { RecipeItemComponent } from './recipeBook/recipe-item/recipe-item.component';
-import { RecipeDetailComponent } from './recipeBook/recipe-detail/recipe-detail.component';
+import { RecipesComponent } from './recipes/recipes.component';
+import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
+import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
+import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    ShoppingListComponent,
-    ShopList,
-    ListEditComponent,
-    RecipeBookComponent,
+    RecipesComponent,
+    RecipeListComponent,
+    RecipeDetailComponent,
     RecipeItemComponent,
-    RecipeDetailComponent
+    ShoppingListComponent,
+    ShoppingEditComponent
   ],
   imports: [
     BrowserModule
